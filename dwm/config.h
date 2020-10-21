527934cb2be9c85e26a99f4bf035103c5cec6413
GrapeJuiceSoda <kcnanmoto@berkeley.edu>
2020-10-20 22:29:50 -0700

    Master layout

--- dwm/config.h
+++ dwm/config.h
@@ -13,7 +13,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int statmonval         = 0;        /*Status Bar stays on the top screen*/
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "SourceCodePro:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -52,6 +52,8 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 	{ "(@)",      spiral },
 	{ "[\\]",     dwindle },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -68,7 +70,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan2, "-sf", col_gray1, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "termite", NULL };
 static const char *waterfox[]  = { "waterfox-current", NULL };
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
@@ -106,6 +108,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ ControlMask|ShiftMask,        XK_f,      setlayout,      {.v = &layouts[3]} },
 	{ ControlMask|ShiftMask,        XK_r,      setlayout,      {.v = &layouts[4]} },
+  { ControlMask|ShiftMask,        XK_u,      setlayout,      {.v = &layouts[5]} },
+	{ ControlMask|ShiftMask,        XK_o,      setlayout,      {.v = &layouts[6]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
